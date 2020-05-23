af0e0c389bd792950b91c7573459afbacbe5a562
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2020-05-23 22:54:29 +0200

    Entity ser/deser finetuned + test + launch config fix"

--- src/gdf_agent.rs
+++ src/gdf_agent.rs
@@ -4,17 +4,26 @@ use serde_json;
 #[allow(unused_imports)]
 use crate::errors::Result;
 
-// TBD: https://serde.rs/field-attrs.html
-#[allow(non_snake_case)]
+// see https://serde.rs/field-attrs.html
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Entity {
     pub id: String,
     pub name: String,
-    pub isOverridable: bool,
-    pub isEnum: bool,
-    pub isRegexp: bool,
-    pub automatedExpansion: bool,
-    pub allowFuzzyExtraction: bool
+    
+    #[serde(rename = "isOverridable")]
+    pub is_overridable: bool,
+    
+    #[serde(rename = "isEnum")]
+    pub is_enum: bool,
+    
+    #[serde(rename = "isRegexp")]
+    pub is_regexp: bool,
+    
+    #[serde(rename = "automatedExpansion")]
+    pub automated_expansion: bool,
+
+    #[serde(rename = "allowFuzzyExtraction")]
+    pub allow_fuzzy_extraction: bool
 }
 
 #[cfg(test)]
@@ -22,7 +31,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_entity_from_str() -> Result<()> {
+    fn test_entity_deser_ser() -> Result<()> {
 
         let entity_str = r#"
         {
@@ -38,12 +47,15 @@ mod tests {
         let entity: Entity = serde_json::from_str(entity_str)?;
         assert_eq!(entity.id,  "ed3dad98-49c6-4370-9f7e-0c6648d99820");
         assert_eq!(entity.name,  "additional");
-        assert_eq!(entity.isOverridable,  true);
-        assert_eq!(entity.isEnum,  true);
-        assert_eq!(entity.isRegexp,  false);
-        assert_eq!(entity.automatedExpansion,  false);
-        assert_eq!(entity.allowFuzzyExtraction,  false);
-        
+        assert_eq!(entity.is_overridable,  true);
+        assert_eq!(entity.is_enum,  true);
+        assert_eq!(entity.is_regexp,  false);
+        assert_eq!(entity.automated_expansion,  false);
+        assert_eq!(entity.allow_fuzzy_extraction,  false);
+
+        let serialized_str = serde_json::to_string(&entity).unwrap();
+        let serialized_str_expected = r#"{"id":"ed3dad98-49c6-4370-9f7e-0c6648d99820","name":"additional","isOverridable":true,"isEnum":true,"isRegexp":false,"automatedExpansion":false,"allowFuzzyExtraction":false}"#;
+        assert_eq!(serialized_str,  serialized_str_expected);
         Ok(())
     }
 }
