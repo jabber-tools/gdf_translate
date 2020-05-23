1d2753266449687e08a7c9434e720e057ca3f4df
Adam Bezecny (DHL IT Services) <Adam.Bezecny@dhl.com>
2020-05-24 00:27:36 +0200

    serde entity creation - wip

--- src/gdf_agent.rs
+++ src/gdf_agent.rs
@@ -32,9 +32,155 @@ pub struct EntityEntry {
     pub synonyms: Vec<String>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Package {
+    pub version: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AgentManifestGoogleAssistantOauthLinking {
+    pub required: bool,
+
+    #[serde(rename = "providerId")]
+    pub provider_id: String,
+
+    #[serde(rename = "authorizationUrl")]
+    pub authorization_url: String,
+
+    #[serde(rename = "tokenUrl")]
+    pub token_url: String,
+
+    pub scopes: String,
+
+    #[serde(rename = "privacyPolicyUrl")]
+    pub privacy_policy_url: String,
+
+    #[serde(rename = "grantType")]
+    pub grant_type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AgentManifestGoogleAssistant {
+    #[serde(rename = "googleAssistantCompatible")]
+    pub google_assistant_compatible: bool,
+
+    pub project: String,
+
+    #[serde(rename = "welcomeIntentSignInRequired")]
+    pub welcome_intent_signin_required: bool,
+
+    #[serde(rename = "startIntents")]
+    pub start_intents: Vec<String>,
+
+    #[serde(rename = "systemIntents")]
+    pub system_intents: Vec<String>,
+
+    #[serde(rename = "endIntentIds")]
+    pub end_intent_ids: Vec<String>,
+
+    #[serde(rename = "oAuthLinking")]
+    pub oauth_linking: AgentManifestGoogleAssistantOauthLinking,
+
+    #[serde(rename = "voiceType")]
+    pub voice_type: String,
+
+    pub capabilities: Vec<String>, // ??
+    pub env: String,
+
+    #[serde(rename = "protocolVersion")]
+    pub protocol_version: String,
+
+    #[serde(rename = "autoPreviewEnabled")]
+    pub auto_preview_enabled: bool,
+
+    #[serde(rename = "isDeviceAgent")]
+    pub is_device_agent: bool,
+}
+
+// TBD: there will be probably field for online code if enabled
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AgentManifestWebhook {
+    url: String,
+    username: String,
+    headers: std::collections::HashMap<String, String>,
+    available: bool,
+
+    #[serde(rename = "useForDomains")]
+    use_for_domains: bool,
+
+    #[serde(rename = "cloudFunctionsEnabled")]
+    cloud_functions_enabled: bool,
+
+    #[serde(rename = "cloudFunctionsInitialized")]
+    cloud_functions_initialized: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AgentManifest {
+    pub description: String,
+    pub language: String,
+
+    #[serde(rename = "shortDescription")]
+    pub short_description: String,
+
+    pub examples: String,
+
+    #[serde(rename = "linkToDocs")]
+    pub link_to_docs: String,
+
+    #[serde(rename = "disableInteractionLogs")]
+    pub disable_interaction_logs: bool,
+
+    #[serde(rename = "disableStackdriverLogs")]
+    pub disable_stackdriver_logs: bool,
+
+    #[serde(rename = "googleAssistant")]
+    pub google_assistant: AgentManifestGoogleAssistant,
+
+    #[serde(rename = "defaultTimezone")]
+    pub default_timezone: String,
+
+    pub webhook: AgentManifestWebhook,
+
+    #[serde(rename = "isPrivate")]
+    is_private: bool,
+
+    #[serde(rename = "customClassifierMode")]
+    pub custom_classifier_mode: String,
+
+    #[serde(rename = "mlMinConfidence")]
+    pub ml_min_confidence: f64,
+
+    #[serde(rename = "supportedLanguages")]
+    pub supported_languages: Vec<String>,
+
+    #[serde(rename = "onePlatformApiVersion")]
+    pub one_platform_api_version: String,
+
+    #[serde(rename = "analyzeQueryTextSentiment")]
+    pub analyze_query_text_sentiment: bool,
+
+    #[serde(rename = "enabledKnowledgeBaseNames")]
+    pub enabled_knowledge_base_names: Vec<String>,
+
+    #[serde(rename = "knowledgeServiceConfidenceAdjustment")]
+    pub knowledge_service_confidence_adjustment: f64,
+
+    #[serde(rename = "dialogBuilderMode")]
+    pub dialog_builder_mode: bool,
+
+    #[serde(rename = "baseActionPackagesUrl")]
+    pub base_action_packages_url: String,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn remove_whitespace(s: &str) -> String {
+        let normalized_str: String = s.split_whitespace().collect();
+        normalized_str.replace("\n", "")
+    }
 
     #[test]
     fn test_entity_deser_ser() -> Result<()> {
@@ -135,6 +281,102 @@ mod tests {
         let serialized_str_expected = r#"[{"value":"(?i)j?jd[01]{2}\\d{14,16}","synonyms":[]}]"#;
         assert_eq!(serialized_str, serialized_str_expected);
 
+        Ok(())
+    }
+
+    #[test]
+    fn test_package_deser_ser() -> Result<()> {
+        let package_str = r#"
+        {
+            "version": "1.0.0"
+        }
+        "#;
+        let package: Package = serde_json::from_str(package_str)?;
+        assert_eq!(package.version, "1.0.0");
+
+        let serialized_str = serde_json::to_string(&package).unwrap();
+        let serialized_str_expected = r#"{"version":"1.0.0"}"#;
+        assert_eq!(
+            remove_whitespace(&serialized_str),
+            remove_whitespace(serialized_str_expected)
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_agent_manifest_deser_ser() -> Result<()> {
+        let agent_str = r#"
+        {
+            "description": "Express tracking bot",
+            "language": "en",
+            "shortDescription": "",
+            "examples": "",
+            "linkToDocs": "",
+            "disableInteractionLogs": false,
+            "disableStackdriverLogs": false,
+            "googleAssistant": {
+              "googleAssistantCompatible": true,
+              "project": "express-tracking",
+              "welcomeIntentSignInRequired": true,
+              "startIntents": [],
+              "systemIntents": [],
+              "endIntentIds": [],
+              "oAuthLinking": {
+                "required": false,
+                "providerId": "",
+                "authorizationUrl": "",
+                "tokenUrl": "",
+                "scopes": "",
+                "privacyPolicyUrl": "",
+                "grantType": "AUTH_CODE_GRANT"
+              },
+              "voiceType": "MALE_1",
+              "capabilities": [],
+              "env": "",
+              "protocolVersion": "V2",
+              "autoPreviewEnabled": false,
+              "isDeviceAgent": false
+            },
+            "defaultTimezone": "Europe/Madrid",
+            "webhook": {
+              "url": "https://express-cs-enterprise.appspot.com/fulfilment",
+              "username": "",
+              "headers": {
+                "": ""
+              },
+              "available": true,
+              "useForDomains": false,
+              "cloudFunctionsEnabled": false,
+              "cloudFunctionsInitialized": false
+            },
+            "isPrivate": true,
+            "customClassifierMode": "use.after",
+            "mlMinConfidence": 0.5,
+            "supportedLanguages": [
+              "es",
+              "pt-br"
+            ],
+            "onePlatformApiVersion": "v2",
+            "analyzeQueryTextSentiment": true,
+            "enabledKnowledgeBaseNames": [],
+            "knowledgeServiceConfidenceAdjustment": -0.4,
+            "dialogBuilderMode": false,
+            "baseActionPackagesUrl": ""
+          }
+        "#;
+        let agent: AgentManifest = serde_json::from_str(agent_str)?;
+        assert_eq!(agent.description, "Express tracking bot");
+        assert_eq!(agent.knowledge_service_confidence_adjustment, -0.4);
+        assert_eq!(
+            agent.google_assistant.oauth_linking.grant_type,
+            "AUTH_CODE_GRANT"
+        );
+
+        let serialized_str = serde_json::to_string(&agent).unwrap();
+        assert_eq!(
+            remove_whitespace(&serialized_str),
+            remove_whitespace(agent_str)
+        );
         Ok(())
     }
 }
